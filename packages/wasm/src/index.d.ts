7f4a2f1bebc297ef632defef3d9d52512c68f6d8
Jeremy Banka <hello@jeremybanka.com>
2026-05-24 01:48:15 -0700

    🧹 clean up old build

--- packages/wasm/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export interface FormatOptions {
-  indentWidth?: number;
-  lineWidth?: number;
-  maxBlankLines?: number;
-}
-export declare function formatText(fileText: string, options?: FormatOptions): string;
